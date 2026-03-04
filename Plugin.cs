befa8a1be32e70036607075bef6546e96b143baa
Lupey <irmanvelkic@gmail.com>
2026-03-03 19:49:17 -0600

    alright added default files for the mod

---
+++ Plugin.cs
@@ -0,0 +1,12 @@
+﻿using BepInEx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CustomSkyBoxMod
+{
+    [BepInPlugin("Lupeydev.CustomSkyBoxMod", "CustomSkyBoxMod", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+       
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Custom-SkyBox-MOD
-=
+# CUSTOM SKYBOX MOD
+1. Make a folder named CustomSkyBox 
+2. Put the .dll in the folder
+3. Add any picture of your choice
+4. Enjoy!
+
+## Warning
+Mods can ban you from Gorilla Tag
